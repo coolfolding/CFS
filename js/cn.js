01a95978f1d414ed4447cee70ad6a3c416563cb6
coolfolding <69616412+coolfolding@users.noreply.github.com>
2022-07-15 17:29:16 +0800

    Add files via upload

---
+++ js/cn.js
@@ -0,0 +1,9 @@
+   
+   $(document).ready(function()
+   {
+      $("a[href*='#header']").click(function(event)
+      {
+         event.preventDefault();
+         $('html, body').stop().animate({ scrollTop: $('#wb_header').offset().top }, 600, 'easeOutCirc');
+      });
+   });
